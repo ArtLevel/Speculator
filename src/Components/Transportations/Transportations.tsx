eeeb48b9e90f9968d150588dcde0305bb6ec0294
ArtLevel <algitit@rambler.ru>
2025-06-02 12:28:00 +0300

    Added styles for transportations.tsx

--- src/Components/Transportations/Transportations.tsx
+++ src/Components/Transportations/Transportations.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 
-import "./Transportations.css";
+import "./Transportations.scss";
 
 function Transportations() {
   return (
-    <div>
+    <div className="transportations">
       <h2 className="title">Активные перевозки</h2>
-      <div className="panel">перевозки</div>
+      <div className="panel">
+        <div className="good-item-wrapper">
+          <div className="good-item-description">
+            <div className="good-item item-1">d</div>
+          </div>
+
+          <div className="good-item-transport-info">
+            <div>
+              <div className="header">Пиво</div>
+              <div className="path">Москва - Пекин</div>
+            </div>
+            <div>
+              <div className="days">Дни: 12</div>
+              <button className="" disabled={true}>
+                Получить
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
